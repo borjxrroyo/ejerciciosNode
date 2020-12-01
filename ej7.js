0672ab1ce0ca0141f30bea02a46442d23f0e896e
Borja Arroyo Manresa <72812865+borjxrroyo@users.noreply.github.com>
2020-12-01 15:26:28 +0100

    Diferencias entre GET y POST
    Explicación de las diferencias entre el método GET y el método POST

--- ej7.js
+++ ej7.js
@@ -6,7 +6,7 @@ app.use(express.urlencoded({ extended: true }))
 
 app.post('/bienvenida/', (req, res) => {
 console.log(req.body.nombre)
-const cadena = `Hola! ${req.body.nombre}`           // Se define una constante "cadena" que incluye un string con "Hola! " + la variable "nombre" pedida en la barra de direcciones
+const cadena = `Hola! ${req.body.nombre}`           // Variables en BODY porque es tipo POST. Así no se alarga la URL - Se define una constante "cadena" que incluye un string con "Hola! " + la variable "nombre" pedida en la barra de direcciones
 res.send(cadena)                                    // Guarda y muestra por pantalla una constante "cadena" con el saludo y el nombre introducido
 })
 app.get('/despedida/', (req, res) => {                   // La aplicación utiliza el apartado "despedida" de Localhost:5000 -> Como si fuera http://miweb.com/despedida/
@@ -16,3 +16,5 @@ res.send(cadena)                                    // Muestra por pantalla una 
 app.listen(5000, '127.0.0.1', () => console.log('Servidor preparado: 127.0.0.1:5000'))  // La aplicación manda un mensaje de CONSOLA conforme el servidor se está ejecutando en la dirección indicada
 
 // Para detener el servidor se debe utilizar Ctrl + C
+
+// GET: Las variables se ubican en la barra de direcciones
